a37286b096b2baee19248ae09c7908438abe95c2
Vlad <61822530+VladOnline@users.noreply.github.com>
2020-04-23 17:42:58 +0300

    GUIMAin

---
+++ src/GUIMain.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUIMain {
+
+        static JFrame frame = new JFrame();
+        static   JPanel jPanel = new JPanel();
+    JButton jButton =new JButton("Start");
+    JButton jButton1 =new JButton("Options");
+    JButton jButton2 =new JButton("Exit");
+
+    GUIMain(){
+        frame = getFrame();
+        frame.add(jPanel);
+        jPanel.add(jButton);
+        jPanel.add(jButton1);
+        jPanel.add(jButton2);
+        jPanel.setLayout(new GridLayout(3,0,1,1));
+            jPanel.revalidate();
+
+        }
+
+        static JFrame getFrame(){
+            frame.setVisible(true);
+            frame.setResizable(true);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setTitle("my app");
+            Toolkit toolkit = Toolkit.getDefaultToolkit();
+            Dimension dimension = toolkit.getScreenSize();
+            frame.setBounds(dimension.width / 2 - 125, dimension.height / 2 - 125, 250, 250);
+            return frame;
+        }
+    }
+
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,5 @@
 public class Main {
+    public static void main(String[] args){
+        GUIMain GUIMain = new GUIMain();
+    }
 }
